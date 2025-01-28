00f329304c4291a7985b7eff1ac46938e85cde0f
ZabWP <zabdiel.bronola01@gmail.com>
2025-01-28 14:21:10 -0500

    added a modular text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { DescriptionBox } from "./components/DescriptionBox";
 
 const App = () => {
   return (
     <div>
       <h1 className="Title">React App</h1>
+      <DescriptionBox />
     </div>
   );
 };
---
+++ frontend/src/components/DescriptionBox.jsx
@@ -0,0 +1,7 @@
+export const DescriptionBox = () => {
+  return (
+    <div>
+      <p>hello this is some sample modular text</p>
+    </div>
+  );
+};
