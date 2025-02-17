bfc7bd61e9034edb995579f9e96da5f4556a1e0a
ZabWP <zabdiel.bronola01@gmail.com>
2025-02-17 09:47:35 -0500

    reset

--- frontend/src/components/DescriptionBox.jsx
+++
@@ -1,9 +0,0 @@
-// This is an example of a reusable component
-
-export const DescriptionBox = () => {
-  return (
-    <div>
-      <p>hello this is some sample modular text</p>
-    </div>
-  );
-};
