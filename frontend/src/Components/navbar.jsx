caa940cd262b6d1b1d4720b269ecd2983895e0ae
ZabWP <zabdiel.bronola01@gmail.com>
2025-03-05 11:15:34 -0500

    added profile button on navbar

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -1,9 +1,22 @@
 import SSO from "./googleSSO";
 import "./navbar.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 const Navbar = () => {
 
   const navigate = useNavigate();
+  const [profileImage, setProfileImage] = useState();
+
+  // useEffect(() => {
+  //   // Fetch the profile image URL from an API or local storage
+  //   const fetchProfileImage = async () => {
+  //     const response = await fetch("/api/profile-image");
+  //     const data = await response.json();
+  //     setProfileImage(data.imageUrl);
+  //   };
+
+  //   fetchProfileImage();
+  // }, []);
 
   return (
     <div className="navbarContainer">
@@ -13,6 +26,10 @@ const Navbar = () => {
       </div>
      
       <SSO />
+
+      <div className="profileContainer">
+        <img src={profileImage ? profileImage : "https://cdn.discordapp.com/attachments/1346268283279773759/1346874768770596985/blank-profile-picture-973460_1280.png?ex=67c9c660&is=67c874e0&hm=06ed63a7667746d76a0bc1de2f24acbded570777693cc024820afdac53a745ca&"} alt="" />
+      </div>
     </div>
   );
 };
